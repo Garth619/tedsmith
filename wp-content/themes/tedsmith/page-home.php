90d63ebbbd7ae4201164c07f8eca5c45e1a66625
Garrett Cullen <garrettwcullen@gmail.com>
2016-07-08 15:12:34 -0700

    push

--- wp-content/themes/tedsmith/page-home.php
+++ wp-content/themes/tedsmith/page-home.php
@@ -6,6 +6,17 @@
 get_header(); ?>
 
 
+<div class="home_banner">
+	
+	
+	<img class="play_button" src="<?php bloginfo('template_directory');?>/images/mobile-play.png"/>
+	
+	<h1>Serving the citizens
+of Texas since&nbsp;1976</h1>
+	
+	
+</div><!-- home_banner -->
+
 
 
 <?php get_footer(); ?>
