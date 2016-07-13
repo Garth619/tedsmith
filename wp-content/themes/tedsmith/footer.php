8949131ddbbd90b85ccb77e30f5000cef946a351
Garrett Cullen <garrettwcullen@gmail.com>
2016-07-13 08:40:10 -0700

    push

--- wp-content/themes/tedsmith/footer.php
+++ wp-content/themes/tedsmith/footer.php
@@ -1,4 +1,66 @@
-
+<section class="request_wrapper">
+		
+		<div class="request">
+			
+			<h1>request<br/>a free consultation</h1>
+			
+		</div><!-- request -->
+		
+		<div class="form_wrapper">
+			
+			<?php gravity_form(1, false, true, false, '', true, 12);?>
+			
+		</div><!-- form_wrapper -->
+		
+	</section><!-- request_wrapper -->
+	
+	<section class="office">
+		
+		<span class="office_titles">office</span>
+		<span class="sub_office_titles">660 West FM 2410 Harker Heights, Tx 76548</span>
+		
+		<div class="gradient_button"><a href="">Get Directions</a></div><!-- gradient_button -->
+		
+		<div class="gradient_divider"></div><!-- gradient_divider -->
+		
+		<span class="office_titles">phone</span>
+		<a href="tel:2546905688" class="sub_office_titles">254.690.5688</a>
+		
+		<span class="office_titles fax_margin">fax</span>
+		<a href="tel:2546905685" class="sub_office_titles">254.690.5685</a>
+		
+		<div class="gradient_divider"></div><!-- gradient_divider -->
+		
+		<span class="office_titles">we accept</span>
+		
+		<img class="payment" src="<?php bloginfo('template_directory');?>/images/small-imgs/paypal.png"/>
+		
+		<img class="payment" src="<?php bloginfo('template_directory');?>/images/small-imgs/creditcards.png"/>
+		
+	</section><!-- office -->
+	
+	<section class="social_icons">
+		
+		<div class="social_icon_wrapper">
+			
+			<a href="">
+				<img class="social_img" src="<?php bloginfo('template_directory');?>/images/small-imgs/social-goggle.png"/>
+				<span>Google+</span>
+			</a>
+			
+		</div><!-- social_icon_wrapper -->
+		
+		
+	</section><!-- social_icons -->
+	
+	<footer>
+		
+		
+		<span class="copyright">Copyright &copy; ted smith law group 2016 | <a href="">disclaimer</a> | <a href="">sitemap</a></span>
+		<a href=""><img src="<?php bloginfo('template_directory');?>/images/small-imgs/ilawyer-logo.png"/></a>
+		
+		
+	</footer>
 	
 <?php wp_footer(); ?>
 <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/tedsmith-custom-min.js"></script>
