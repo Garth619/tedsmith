a6e6ba466dfd5637232d99a127814a704a951ade
Garrett Cullen <garrettwcullen@gmail.com>
2016-07-19 10:52:47 -0700

    push

--- wp-content/themes/tedsmith/page-home.php
+++ wp-content/themes/tedsmith/page-home.php
@@ -325,12 +325,28 @@ get_header(); ?>
 		
 		<div class="testimonial_wrapper">
 			
-			<img class="testimonial_stars" src="<?php bloginfo('template_directory');?>/images/small-imgs/test-stars.png"/>
+			<div class="single_testimonial">
+				
+				<img class="testimonial_stars" src="<?php bloginfo('template_directory');?>/images/small-imgs/test-stars.png"/>
 			
-			<p>"The Ted Smith Law Group is one of the most community minded firms I have ever seen. The way they give back to the community is way over and above the call of duty. In my opinion, if you want the best, you cannot go wrong with this&nbsp;group."</p>
+				<p>"The Ted Smith Law Group is one of the most community minded firms I have ever seen. The way they give back to the community is way over and above the call of duty. In my opinion, if you want the best, you cannot go wrong with this&nbsp;group."</p>
 			
-			<span class="client_subheader">Personal Injury Client</span>
-			<span class="client_lowerheader">client direct review</span>
+				<span class="client_subheader">Personal Injury Client</span>
+				<span class="client_lowerheader">client direct review</span>
+			
+			</div><!-- single_testimonial -->
+			
+			<div class="single_testimonial">
+				
+				<img class="testimonial_stars" src="<?php bloginfo('template_directory');?>/images/small-imgs/test-stars.png"/>
+			
+				<p>“Mr. Smith took my case and won. I'm very grateful with Mr. Smith. He took the time to review all my files and explain to me what he was doing and our chances. 
+His very professional at what he do. I recommend Mr. Smith to anyone who needs legal help.”</p>
+			
+				<span class="client_subheader">Juan</span>
+				<span class="client_lowerheader">avvo.com review</span>
+			
+			</div><!-- single_testimonial -->
 			
 		</div><!-- testimonial_wrapper -->
 		
