6e5382dfc036e0104bd2072ab31c9ac964401187
Garrett Cullen <garrettwcullen@gmail.com>
2016-08-02 12:26:51 -0700

    push

--- wp-content/themes/tedsmith/sidebar.php
+++ wp-content/themes/tedsmith/sidebar.php
@@ -1,3 +1,7 @@
 <div id="sidebar">
-	sidebar
+	
+	<div class="gradient_divider"></div><!-- gradient_divider -->
+	
+	<?php wp_nav_menu( array( 'container_class' => 'sidebar_menu_wrapper', 'theme_location' => 'sidebar' ) ); ?>
+
 </div><!-- sidebar -->
