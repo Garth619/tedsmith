61bd735eedb715c1982fc1814d5a2a47d6f10f69
garrett cullen <info@garrettcullen.com>
2016-08-03 05:28:28 -0700

    push

--- wp-content/themes/tedsmith/page-caseresults.php
+++ wp-content/themes/tedsmith/page-caseresults.php
@@ -61,6 +61,17 @@ get_header(); ?>
 				
 			</div><!-- single_case_result -->
 			
+			<div class="single_case_result">
+				
+				
+				<span class="case_number">$1,500,000</span><!-- case_number -->
+				<span class="case_subheader">Prescription Error Settlement</span><!-- case_subheader -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, justo vulputate lobortis pellentesque, turpis lorem efficitur ante, sit amet auctor nibh purus a orci. Quisque ut elit viverra, luctus lorem fringilla, sodales diam. Nunc egestas est id lacus malesuada, nec rutrum mi&nbsp;pulvinar.</p>
+				
+				
+			</div><!-- single_case_result -->
+			
 		</div><!-- case_results_wrapper -->
 		
 
