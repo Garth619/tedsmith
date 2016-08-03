690fd4d9f8c722e465f43160b22ee5af5a6d4a67
garrett cullen <info@garrettcullen.com>
2016-08-02 23:01:40 -0700

    push

---
+++ wp-content/themes/tedsmith/page-caseresults.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Template Name: Case Results
+ *
+ */
+
+get_header(); ?>
+
+<div class="main">
+	
+	<?php include('banner.php');?>
+	
+	<div class="content">
+	
+		<h1><?php the_title(); ?></h1>
+		<div class="gradient_divider"></div><!-- gradient_divider -->
+		
+		<div class="case_results_wrapper">
+			
+			<div class="single_case_result">
+				
+				
+				<span class="case_number">$1,500,000</span><!-- case_number -->
+				<span class="case_subheader">Prescription Error Settlement</span><!-- case_subheader -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, justo vulputate lobortis pellentesque, turpis lorem efficitur ante, sit amet auctor nibh purus a orci. Quisque ut elit viverra, luctus lorem fringilla, sodales diam. Nunc egestas est id lacus malesuada, nec rutrum mi&nbsp;pulvinar.</p>
+				
+				
+			</div><!-- single_case_result -->
+			
+			<div class="single_case_result">
+				
+				
+				<span class="case_number">$1,500,000</span><!-- case_number -->
+				<span class="case_subheader">Prescription Error Settlement</span><!-- case_subheader -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, justo vulputate lobortis pellentesque, turpis lorem efficitur ante, sit amet auctor nibh purus a orci. Quisque ut elit viverra, luctus lorem fringilla, sodales diam. Nunc egestas est id lacus malesuada, nec rutrum mi&nbsp;pulvinar.</p>
+				
+				
+			</div><!-- single_case_result -->
+			
+			<div class="single_case_result">
+				
+				
+				<span class="case_number">$1,500,000</span><!-- case_number -->
+				<span class="case_subheader">Prescription Error Settlement</span><!-- case_subheader -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, justo vulputate lobortis pellentesque, turpis lorem efficitur ante, sit amet auctor nibh purus a orci. Quisque ut elit viverra, luctus lorem fringilla, sodales diam. Nunc egestas est id lacus malesuada, nec rutrum mi&nbsp;pulvinar.</p>
+				
+				
+			</div><!-- single_case_result -->
+			
+			<div class="single_case_result">
+				
+				
+				<span class="case_number">$1,500,000</span><!-- case_number -->
+				<span class="case_subheader">Prescription Error Settlement</span><!-- case_subheader -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, justo vulputate lobortis pellentesque, turpis lorem efficitur ante, sit amet auctor nibh purus a orci. Quisque ut elit viverra, luctus lorem fringilla, sodales diam. Nunc egestas est id lacus malesuada, nec rutrum mi&nbsp;pulvinar.</p>
+				
+				
+			</div><!-- single_case_result -->
+			
+		</div><!-- case_results_wrapper -->
+		
+
+	</div><!-- content -->
+
+</div><!-- main -->
+
+<?php get_footer(); ?>
