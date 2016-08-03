509125976ffecbb5b1e34c2586446e4b9e865b65
Garrett Cullen <garrettwcullen@gmail.com>
2016-08-03 14:28:37 -0700

    push

--- wp-content/themes/tedsmith/page-videos.php
+++ wp-content/themes/tedsmith/page-videos.php
@@ -15,7 +15,41 @@ get_header(); ?>
 		<h1><?php the_title(); ?></h1>
 		<div class="gradient_divider"></div><!-- gradient_divider -->
 		
-		<div class="video_wrapper"></div>
+		<div class="video_wrapper">
+			
+			<div class="single_video">
+			
+				<script src="//fast.wistia.com/embed/medias/14oizzbrxp.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_14oizzbrxp popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<span class="video_title">Family Law | Ted Smith Law Group, PLLC</span><!-- title -->
+			
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+			
+				<script src="//fast.wistia.com/embed/medias/14oizzbrxp.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_14oizzbrxp popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<span class="video_title">Family Law | Ted Smith Law Group, PLLC</span><!-- title -->
+			
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+			
+				<script src="//fast.wistia.com/embed/medias/14oizzbrxp.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_14oizzbrxp popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<span class="video_title">Family Law | Ted Smith Law Group, PLLC</span><!-- title -->
+			
+			</div><!-- single_video -->
+			
+			<div class="single_video">
+			
+				<script src="//fast.wistia.com/embed/medias/14oizzbrxp.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_14oizzbrxp popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<span class="video_title">Family Law | Ted Smith Law Group, PLLC</span><!-- title -->
+			
+			</div><!-- single_video -->
+			
+		</div><!-- video_wrapper -->
 		
 
 	</div><!-- content -->
