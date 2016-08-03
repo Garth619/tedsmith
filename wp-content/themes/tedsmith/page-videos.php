5e9309d82c7beee578e0e6763bf0f33d0127aabd
Garrett Cullen <garrettwcullen@gmail.com>
2016-08-03 14:07:35 -0700

    push

---
+++ wp-content/themes/tedsmith/page-videos.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Videos
+ *
+ */
+
+get_header(); ?>
+
+<div class="main">
+	
+	<?php include('banner.php');?>
+	
+	<div class="content">
+	
+		<h1><?php the_title(); ?></h1>
+		<div class="gradient_divider"></div><!-- gradient_divider -->
+		
+		<div class="video_wrapper"></div>
+		
+
+	</div><!-- content -->
+
+</div><!-- main -->
+
+<?php get_footer(); ?>
