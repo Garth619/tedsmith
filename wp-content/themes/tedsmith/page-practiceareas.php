8811c55734df3519f5699dc5962f8071d85e698a
garrett cullen <info@garrettcullen.com>
2016-08-03 19:07:00 -0700

    push

---
+++ wp-content/themes/tedsmith/page-practiceareas.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Template Name: Practice Areas
+ *
+ */
+
+get_header(); ?>
+
+<div class="main">
+	
+	<?php include('banner.php');?>
+	
+	<div class="content">
+	
+		<h1><?php the_title(); ?></h1>
+		<div class="gradient_divider"></div><!-- gradient_divider -->
+		
+		<div class="practice_wrapper">
+			
+			
+						<div class="pa_col">
+							
+							<ul>
+								<li><a href="">personal injury</a></li>
+								<li><a href="">family law</a></li>
+								<li><a href="">overview</a></li>
+								<li><a href="">divorce</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">social security disability</a></li>
+							</ul>
+							
+						</div><!-- pa_col -->
+						
+						<div class="pa_col">
+							
+							<ul>
+								<li><a href="">personal injury</a></li>
+								<li><a href="">family law</a></li>
+								<li><a href="">overview</a></li>
+								<li><a href="">divorce</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">social security disability</a></li>
+							</ul>
+							
+						</div><!-- pa_col -->
+						
+						<div class="pa_col">
+							
+							<ul>
+								<li><a href="">personal injury</a></li>
+								<li><a href="">family law</a></li>
+								<li><a href="">overview</a></li>
+								<li><a href="">divorce</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">social security disability</a></li>
+							</ul>
+							
+						</div><!-- pa_col -->
+						
+						<div class="pa_col">
+							
+							<ul>
+								<li><a href="">personal injury</a></li>
+								<li><a href="">family law</a></li>
+								<li><a href="">overview</a></li>
+								<li><a href="">divorce</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">child custody</a></li>
+								<li><a href="">social security disability</a></li>
+							</ul>
+							
+						</div><!-- pa_col -->	
+			
+			
+			
+			
+			
+		</div><!-- practice_wrapper -->
+		
+
+	</div><!-- content -->
+
+</div><!-- main -->
+
+<?php get_footer(); ?>
