cba0673922bbaeb869bd0f7bc38d705f220dc869
Garrett Cullen <garrettwcullen@gmail.com>
2016-09-09 16:01:13 -0700

    push

--- wp-content/themes/tedsmith/page-about.php
+++ wp-content/themes/tedsmith/page-about.php
@@ -12,7 +12,32 @@ get_header(); ?>
 	
 	<div class="content">
 	
-		<?php get_template_part( 'loop', 'page' ); ?>
+		
+		<h1><?php the_title(); ?></h1>
+		<div class="gradient_divider"></div><!-- gradient_divider -->
+		
+		
+		<div class="about_inner_wrapper">
+			
+			<div class="about_inner_content">
+			
+				<?php the_field('about_text');?>
+			
+			</div><!-- about_inner_content -->
+			
+			<div class="about_photo_wrapper">
+				
+				
+				<?php $aboutphoto = wp_get_attachment_image_src(get_field('photo'), 'aboutphoto'); ?>
+    	   <img src="<?php echo $aboutphoto[0]; ?>"/>
+    	   
+    	   <span class="about_caption"><?php the_field('photo_caption');?></span><!-- about_caption -->
+				
+				 <div class="gradient_divider"></div>
+			
+			</div><!-- about_photo_wrapper -->
+		
+		</div><!-- about_inner_wrapper -->
 
 	</div><!-- content -->
 
